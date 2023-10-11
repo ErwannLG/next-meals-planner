5ce8398d70ad8de112be4bd4b73d4916ceff2fd5
ErwannLG <erwannj.legall@gmail.com>
2023-10-11 11:05:43 +0200

    fix: DayType in SelectedDaysProvider & tailwind classes order

--- src/components/SelectedDaysProvider.tsx
+++ src/components/SelectedDaysProvider.tsx
@@ -1,74 +1,74 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { Day, Days } from '@/types'
+import { DayType } from "@/types";
+import React from "react";
 
 type SelectedDaysType = {
-	days: Days
-	setDays: React.Dispatch<React.SetStateAction<Days>>
-	selectedDays: string[]
-}
+  days: DayType[];
+  setDays: React.Dispatch<React.SetStateAction<DayType[]>>;
+  selectedDays: string[];
+};
 
 const defaultState = {
-	days: [
-		{
-			name: 'lundi',
-			selected: true,
-		},
-		{
-			name: 'mardi',
-			selected: true,
-		},
-		{
-			name: 'mercredi',
-			selected: true,
-		},
-		{
-			name: 'jeudi',
-			selected: true,
-		},
-		{
-			name: 'vendredi',
-			selected: true,
-		},
-		{
-			name: 'samedi',
-			selected: false,
-		},
-		{
-			name: 'dimanche',
-			selected: false,
-		},
-	],
-}
+  days: [
+    {
+      name: "lundi",
+      selected: true,
+    },
+    {
+      name: "mardi",
+      selected: true,
+    },
+    {
+      name: "mercredi",
+      selected: true,
+    },
+    {
+      name: "jeudi",
+      selected: true,
+    },
+    {
+      name: "vendredi",
+      selected: true,
+    },
+    {
+      name: "samedi",
+      selected: false,
+    },
+    {
+      name: "dimanche",
+      selected: false,
+    },
+  ],
+};
 
-const SelectedDaysContext = React.createContext<SelectedDaysType | null>(null)
+const SelectedDaysContext = React.createContext<SelectedDaysType | null>(null);
 
 function SelectedDaysProvider({ children }: { children: React.ReactNode }) {
-	const [days, setDays] = React.useState(defaultState.days)
+  const [days, setDays] = React.useState(defaultState.days);
 
-	// new array of strings with only the selected days as an array of strings: e.g. ['monday', 'thursday']
-	const selectedDays = days
-		.filter((day) => day.selected)
-		.map(({ name }) => name)
+  // new array of strings with only the selected days as an array of strings: e.g. ['monday', 'thursday']
+  const selectedDays = days
+    .filter((day) => day.selected)
+    .map(({ name }) => name);
 
-	return (
-		<SelectedDaysContext.Provider value={{ days, setDays, selectedDays }}>
-			{children}
-		</SelectedDaysContext.Provider>
-	)
+  return (
+    <SelectedDaysContext.Provider value={{ days, setDays, selectedDays }}>
+      {children}
+    </SelectedDaysContext.Provider>
+  );
 }
 
 export function useSelectedDays() {
-	const context = React.useContext(SelectedDaysContext)
+  const context = React.useContext(SelectedDaysContext);
 
-	if (context === undefined) {
-		throw new Error(
-			'useSelectedDays must be used within a SelectedDaysProvider'
-		)
-	}
+  if (context === undefined) {
+    throw new Error(
+      "useSelectedDays must be used within a SelectedDaysProvider",
+    );
+  }
 
-	return context
+  return context;
 }
 
-export default SelectedDaysProvider
+export default SelectedDaysProvider;
